46323180fa2e1570e33002b7e8148d34c8451249
jorgemartins <jorgemartins@Jorge-Martins.local>
2015-03-31 16:55:15 +0100

    added action appear from top action

---
+++ Appear/appear-from-top.js
@@ -0,0 +1,40 @@
+/**
+* Appear: fade from top, 1st time
+* @desc Makes a layer appear from top, by tapping a tappable layer. It will only fade the first time. 
+* @author Jorge MArtins
+* @version 1.0
+*/
+ 
+ 
+ //Gets the trigger layer
+ var enter = getLayerByName('side-dashboard btn');
+ 
+ //Sets the position and opacity for the initial state.
+ var selectedLayer = getSelectedLayer();
+ //selectedLayer.y = selectedLayer.y - 10;
+ //selectedLayer.opacity = 0;
+ 
+ //Delay
+ var delayAmount = 0.1;
+ 
+ //Condition makes it happen only when it has the initial state
+ 
+ //move down animation 
+ var move = createMoveAnimation(selectedLayer);
+ move.basedOn = enter.tap;
+ move.animates = AnimationMode.withDuration;
+ move.easing = EasingCurve.easeInOutQuadratic;
+ move.condition = selectedLayer.id + '.opacity == ' + 0;
+ move.toY = selectedLayer.y + 10;
+ move.duration = 0.2;
+ move.delay = delayAmount;
+ 
+ //fade in animation 
+ var fade = createFadeAnimation(selectedLayer);
+ fade.basedOn = enter.tap;
+ fade.animates = AnimationMode.withDuration;
+ fade.condition = selectedLayer.id + '.opacity == ' + 0;
+ fade.to = 100;
+ fade.duration = 0.2;
+ fade.easing = EasingCurve.easeInOutQuadratic;
+ fade.delay = delayAmount;
